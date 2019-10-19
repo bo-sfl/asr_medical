56fde404a0d5b2310ac8b1b4b4495681a486b9b6
Gadande <gadande@sflscientific.com>
2019-10-19 16:03:27 -0400

    docker

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from werkzeug import secure_filename
 from functions import process
+import os
 
 app = Flask(__name__)
 
@@ -17,11 +18,24 @@ def submit_file():
     if response == 0:
         return 'file %s format is not correct, please use a valid .wav file' %secure_filename(audio.filename)
     else:
-        # save raw file
-        process.save_to_bucket(audio)
+        # save file on local container
+        audio.save('DeepSpeech-mozilla/uploaded_file.wav')
+
+        # then launch deepspeech
+        print("Casting MAGIC!")
+        target_folder = os.getcwd() + "/DeepSpeech-mozilla"
+
+        return process.deepspeech_predict(target_folder, 'uploaded_file.wav')
+
+        # then launch classifier
+        # then extract and output results
+
+#        process.save_to_bucket(audio)
         # process and save the converted file
         # process.convert_for_asr(audio)
-        return 'file %s uploaded successfully' %secure_filename(audio.filename)
+
+        # return 'file %s uploaded successfully' %secure_filename(audio.filename)
+
 
 # @app.route('/send', methods = ['GET', 'POST'])
 # def send():
@@ -32,4 +46,7 @@ def submit_file():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8000)
+
+# for azure app in flask template
+#"https://sfl-all-asr-app.azurewebsites.net/send"
--- functions/process.py
+++ functions/process.py
@@ -1,5 +1,8 @@
 from werkzeug import secure_filename
 from azure.storage.blob import BlockBlobService
+from deepspeech import Model
+import scipy.io.wavfile as wav
+import pdb
 
 
 ALLOWED_EXTENSIONS = {'wav'}
@@ -12,6 +15,24 @@ def check_format(audio):
         return 1
     else:
         return 0
+
+
+def deepspeech_predict(folder, filename):
+
+    N_FEATURES = 25
+    N_CONTEXT = 9
+    BEAM_WIDTH = 500
+    LM_ALPHA = 0.75
+    LM_BETA = 1.85
+
+    ds = Model(folder + '/models/deepspeech-0.5.1/output_graph.pbmm',
+                N_FEATURES, N_CONTEXT,
+                folder + '/models/deepspeech-0.5.1/alphabet.txt',
+                BEAM_WIDTH)
+
+    fs, audio = wav.read(folder + "/" + filename)
+    return ds.stt(audio, fs)
+
 
 def save_to_bucket(audio):
     output = audio.save(secure_filename(audio.filename))
